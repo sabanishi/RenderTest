24b2b88ccbe764eabefa54c832adaa3850fbe90a
sabanishi <84651801+sabanishi@users.noreply.github.com>
2024-09-24 00:10:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import discord
 import os
+import requests
+import json
 from keep_alive import keep_alive
+
+GITHUB_API_URL = "https://api.github.com/repos/{owner}/{repo}/dispatches"
 
 # 必要なintentsを設定
 intents = discord.Intents.default()
@@ -9,14 +13,51 @@ intents.message_content = True  # メッセージの内容にアクセスでき�
 # 接続に必要なオブジェクトを生成
 client = discord.Client(intents=intents)
 
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+GITHUB_API_URL = os.getenv('GITHUB_API_URL')
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+OWNER = os.getenv('OWNER')
+REPO = os.getenv('REPO')
+
+# Webhookで受け取るイベントデータを取得する関数
+def get_webhook_data():
+    return {"event_type": "trigger-workflow", "client_payload": {"key": "value"}}
+
+def trigger_github_action():
+    url = GITHUB_API_URL.format(owner=OWNER, repo=REPO)
+
+    headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+    payload = get_webhook_data()
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+    if response.status_code == 204:
+        print("GitHub Action has been triggered.")
+    else:
+        print(f"Failed to trigger GitHub Action. Status code: {response.status_code}")
+        print(response.text)
+
 @client.event
 async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
     print('ログインしました')
 
 async def reply(message):
-    reply = f'{message.author.mention} さん、こんにちは！'
-    await message.channel.send(reply)
+    #メッセージ中に「Android」という文字列が含まれているかをチェックする
+    if 'Android' in message.content:
+        print('Androidが含まれています')
+        reply = f'{message.author.mention} さん、了解です！'
+        await message.channel.send(reply)
+        #Android用のGitHub Actionをトリガーする
+        trigger_github_action()
+    
+    if 'iOS' in message.content:
+        reply = f'{message.author.mention} さん、こんにちは！'
+        await message.channel.send(reply)
 
 # メッセージ受信時に動作する処理
 @client.event
@@ -24,7 +65,7 @@ async def on_message(message):
     if client.user in message.mentions:
         await reply(message)
 
+
 # Botの起動とDiscordサーバーへの接続
-TOKEN = os.getenv('DISCORD_TOKEN')
 keep_alive()
-client.run(TOKEN)
+client.run(DISCORD_TOKEN)
